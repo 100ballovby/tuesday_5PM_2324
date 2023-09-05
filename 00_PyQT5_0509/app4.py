2bdce6beacde4811ac995d2f463a2d55b469ce0d
greatraksin <greatraksin@icloud.com>
2023-09-05 19:05:38 +0300

    Initial commit

--- 00_PyQT5_0509/app4.py
+++ 00_PyQT5_0509/app4.py
@@ -1,21 +1,36 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
+from random import choice
 
+window_titles = [
+    'My app',
+    'Still my app',
+    'This is surprising',
+    'Something went wrong',
+]
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.times_clicked = 0
         self.setWindowTitle("MyApp")
         self.button_is_checked = True
 
+        self.windowTitleChanged.connect(self.title_changer)
+
         self.button = QPushButton("Push me")
+        self.button.clicked.connect(self.button_pressed)  # связываю кнопку с методом
         self.setCentralWidget(self.button)
 
-    def button_pressed(self):
-        self.button.setText('You already clicked me')
-        self.button.setEnabled(False)
+    def button_pressed(self):  # приемщик сигнала
+        print('Clicked')
+        new_window_title = choice(window_titles)
+        print(f'Setting title: {new_window_title}.')
+        self.setWindowTitle(new_window_title)
 
-        self.setWindowTitle('My oneshot app')
+    def title_changer(self, title):
+        if title == 'Something went wrong':
+            self.button.setDisabled(True)
 
 
 
